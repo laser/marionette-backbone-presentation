dd7a1d78ef8e32756850e42b22d21947540cd4c4
Erin Swenson-Healey <erin@carbonfive.com>
2013-06-26 17:21:27 -0700

    Clearing the search box when route changes

--- example/js/src/views/search.js
+++ example/js/src/views/search.js
@@ -4,6 +4,10 @@
     "events": {
       "keyup input": "_onSearchKeyup"
     },
+    "initialize": function(options) {
+      this.listenTo(InboxClient.app.vent, "route:changed",
+        this._onRouteChanged);
+    },
     "template": (function() {
       var t;
 
@@ -13,13 +17,14 @@
       return t;
     }()),
     "_onClearClicked": function(e) {
-      var $input = this.$el.find("input");
-
-      $input.val("");
+      this.$el.find("input").val("");
 
       InboxClient.app.vent.trigger("search:requested", "");
 
       e.preventDefault();
+    },
+    "_onRouteChanged": function() {
+      this.$el.find("input").val("");
     },
     "_onSearchKeyup": function(e) {
       var $input = this.$el.find("input");
