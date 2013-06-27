21c691e1b5b20ac03b3de1de46bad70a292baab8
Erin Swenson-Healey <erin@carbonfive.com>
2013-06-26 17:21:37 -0700

    Adding some additional test-data

--- example/js/src/service.js
+++ example/js/src/service.js
@@ -10,13 +10,18 @@
     { "subject": "Salutations", "from": "joe007@example.com"     , "body": "Lorem ipsum blah blah" },
     { "subject": "Greetings"  , "from": "trololo@example.com"    , "body": "Trolololololol Yes!!!" },
     { "subject": "GreTrologlo", "from": "joe001@example.com"     , "body": "Lorem ipsum blah blah" },
-    { "subject": "Greetings"  , "from": "erin1@example.com"      , "body": "Lorem ipsum blah blah" },
+    { "subject": "Greetings"  , "from": "erin1@example.com"      , "body": "Lorem ipsum blah blah" }
   ];
 
   var TRASH = [
-    { "subject": "Spam!", "from": "joe001@example.com", "body": "Cash money in your inbox!" },
-    { "subject": "Spam!", "from": "joe002@example.com", "body": "Free food and free movies!" },
-    { "subject": "Spam!", "from": "joe003@example.com", "body": "Cash lewts" },
+    { "subject": "Spam!"            , "from": "joe001@example.com", "body": "Cash money in your inbox!" } ,
+    { "subject": "Spam!"            , "from": "joe002@example.com", "body": "Free food and free movies!" },
+    { "subject": "Not so free"      , "from": "joe003@example.com", "body": "Cash lewts" }                ,
+    { "subject": "Spam!"            , "from": "obama@example.com" , "body": "Cash lewts" }                ,
+    { "subject": "Free for you!"    , "from": "cruyff@example.com", "body": "Cash lewts" }                ,
+    { "subject": "Spam!"            , "from": "ramsey@example.com", "body": "Cash lewts" }                ,
+    { "subject": "Score less goals!", "from": "kuyt@example.com"  , "body": "Cash lewts" }                ,
+    { "subject": "Score more goals" , "from": "vanp@example.com"  , "body": "Cash lewts" }
   ];
 
   InboxClient.service = {
